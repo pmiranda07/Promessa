1451a121d9e6dcc338df5699e34be3a99d6d3f35
Pedro <pedro21fcp@gmail.com>
2020-04-18 16:21:22 +0100

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -3,24 +3,23 @@ import psycopg2
 import datetime
 import statistics
 
-print("Issue Priority?")
-priority = input()
 
 try:
    connection = psycopg2.connect(user="pmiranda", host="localhost", port="5432", database="issues")
    cursor = connection.cursor()
-   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput AND output.priority = '" + priority + "'")
+   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput")
    start = cursor.fetchall() 
-   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'Done' AND b.toString = 'In Progress' INNER JOIN output ON output.id = a.idOutput AND output.priority = '" + priority + "'")
+   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'Done' AND b.toString = 'In Progress' INNER JOIN output ON output.id = a.idOutput")
    finish = cursor.fetchall() 
    start= [i[0] for i in start]
    finish = [n[0] for n in finish]
    diff = [x2 - x1 for (x1, x2) in zip(start, finish)] 
    ts = []
    for x in diff:
-      ts.append(x.total_seconds())
+      if(x.total_seconds() > 300 and x.total_seconds() < 15778462.98):
+         ts.append(x.total_seconds())
 
-
+   
    avg = statistics.mean(ts)
    var = statistics.pvariance(ts)
    sDev = statistics.pstdev(ts)
