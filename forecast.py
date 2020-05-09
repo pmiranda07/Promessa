e256e8c7a898a6b27cea702eea26924191504fe5
Pedro <pedro21fcp@gmail.com>
2020-05-09 16:37:23 +0100

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -14,27 +14,27 @@ try:
 
    ################### Validation Duration ##########################################
 
-   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput AND output.timeestimate IS NOT NULL")
+   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput") # AND output.timeestimate IS NOT NULL")
    validationStart = cursor.fetchall() 
-   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'Done' AND b.toString = 'In Progress' INNER JOIN output ON output.id = a.idOutput AND output.timeestimate IS NOT NULL")
+   cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'Done' AND b.toString = 'In Progress' INNER JOIN output ON output.id = a.idOutput") # AND output.timeestimate IS NOT NULL")
    validationFinish = cursor.fetchall() 
-   cursor.execute("SELECT a.timeestimate FROM output a INNER JOIN changelog b ON a.id = b.idOutput AND b.toString = 'In Progress' INNER JOIN changelog c ON a.id = c.idOutput AND c.toString = 'Done' AND b.idOutput = c.idOutput AND a.timeestimate IS NOT NULL")
-   estimations = cursor.fetchall()
-   cursor.execute("SELECT a.project FROM output a INNER JOIN changelog b ON a.id = b.idOutput AND b.toString = 'In Progress' INNER JOIN changelog c ON a.id = c.idOutput AND c.toString = 'Done' AND b.idOutput = c.idOutput AND a.timeestimate IS NOT NULL")
+   #cursor.execute("SELECT a.timeestimate FROM output a INNER JOIN changelog b ON a.id = b.idOutput AND b.toString = 'In Progress' INNER JOIN changelog c ON a.id = c.idOutput AND c.toString = 'Done' AND b.idOutput = c.idOutput AND a.timeestimate IS NOT NULL")
+   #estimations = cursor.fetchall()
+   cursor.execute("SELECT a.project FROM output a INNER JOIN changelog b ON a.id = b.idOutput AND b.toString = 'In Progress' INNER JOIN changelog c ON a.id = c.idOutput AND c.toString = 'Done' AND b.idOutput = c.idOutput") # AND a.timeestimate IS NOT NULL")
    idList = cursor.fetchall()
    validationStart= [vs[0] for vs in validationStart]
    validationFinish = [vf[0] for vf in validationFinish]
-   estimations = [e[0] for e in estimations]
+   #estimations = [e[0] for e in estimations]
    idList = [il[0] for il in idList]
    validationDiff = [x2 - x1 for (x1, x2) in zip(validationStart, validationFinish)] 
    validation = []
-   validationEstimations = []
+   #validationEstimations = []
    idProject = []
    resolutionDates = []
    for index, x in enumerate(validationDiff):
-      if(x.total_seconds() > 600 and x.total_seconds() < 2629743 and estimations[index] != 0):
+      if(x.total_seconds() > 600 and x.total_seconds() < 2629743): #and estimations[index] != 0):
          validation.append(x.total_seconds())
-         validationEstimations.append(estimations[index])
+         #validationEstimations.append(estimations[index])
          idProject.append(idList[index])
          resolutionDates.append(validationFinish[index])
    
@@ -44,12 +44,12 @@ try:
 
    ################### Validation Takt Time ##########################################
 
-   # cursor.execute("SELECT project FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) = 12 AND EXTRACT(YEAR FROM resolutionDate) = 2016")
+   # cursor.execute("SELECT project FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) > 9 AND EXTRACT(YEAR FROM resolutionDate) = 2019")
    # idList = cursor.fetchall()
    # idList = [il[0] for il in idList]
    # project = np.random.choice(idList)
  
-   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) = 12 AND EXTRACT(YEAR FROM resolutionDate) = 2016 AND project =" + str(project))
+   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) > 9 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
    # validationFinish = cursor.fetchall() 
    # validationFinish = [vf[0] for vf in validationFinish]
    # validationFinish.sort()
@@ -79,8 +79,8 @@ try:
    diff = [x2 - x1 for (x1, x2) in zip(start, finish)] 
    ts = []
    for ind, x in enumerate(diff):
-      if(x.total_seconds() > 600 and x.total_seconds() < 2629743):
-      #if(x.total_seconds() > 600 and x.total_seconds() < 2629743 and finish[int(ind)] < resolutionDates[int(element)]):
+      #if(x.total_seconds() > 300 and x.total_seconds() < 2629743):
+      if(x.total_seconds() > 300 and x.total_seconds() < 2629743 and finish[int(ind)] < resolutionDates[int(element)]):
          ts.append(x.total_seconds())
 
    if(len(ts) < 2):
@@ -91,7 +91,7 @@ try:
 
    ################# TAKT TIME #######################
    
-   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) < 12 AND EXTRACT(YEAR FROM resolutionDate) = 2016 AND project =" + str(project))
+   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) < 10 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
    # resolution = cursor.fetchall() 
    # resolution= [i[0] for i in resolution]
    # resolution.sort()
@@ -139,29 +139,41 @@ try:
 
 
 ################################# Monte Carlo ###################################
-   num_reps = 500
-   num_sim = 1000
-         
-   all_med = []
-   all_dev = []
-   for s in range(num_sim):
-      target = np.random.normal(med, sDev, num_reps).round(2)
-      new_target = []
-      for j in target:
-         if j > 600:
-            new_target.append(j)
-      md = statistics.mean(new_target)
-      sErr = sem(new_target)
-      deviation = sErr * t.ppf((1 + 0.95) / 2. , len(new_target) - 1)
-      all_med.append(md)
-      all_dev.append(deviation)
+   num_reps = 1000
+   #takt = []
+   tf1 = []    # 0 - 5 Days
+   tf2 = []    # 5 - 10 Days
+   tf3 = []    # 10 - 15 Days
+   tf4 = []    # > 15 Days
+   for r in range(num_reps):
+      obj = np.random.choice(ts)
+      #takt.append(obj)
+      if(obj < 432000):
+         tf1.append(obj)
+      elif(obj >= 432000 and obj < 864000):
+         tf2.append(obj)
+      elif(obj >= 864000 and obj < 1296000):
+         tf3.append(obj)
+      elif(obj >= 1296000):
+         tf4.append(obj)
+   md, deviation = 0, 0
+   if(len(tf1) >= len(tf2) and len(tf1) >= len(tf3) and len(tf1) >= len(tf4)):
+      md = statistics.mean(tf1)
+      deviation = sem(tf1) * t.ppf((1 + 0.95) / 2. , len(tf1) - 1)
+   elif(len(tf2) > len(tf1) and len(tf2) > len(tf3) and len(tf2) > len(tf4)):
+      md = statistics.mean(tf2)
+      deviation = sem(tf2) * t.ppf((1 + 0.95) / 2. , len(tf2) - 1)
+   elif(len(tf3) > len(tf1) and len(tf3) > len(tf2) and len(tf3) > len(tf4)):
+      md = statistics.mean(tf3)
+      deviation = sem(tf3) * t.ppf((1 + 0.95) / 2. , len(tf3) - 1)
+   elif(len(tf4) > len(tf1) and len(tf4) > len(tf2) and len(tf4) > len(tf3)):
+      md = statistics.mean(tf4)
+      deviation = sem(tf4) * t.ppf((1 + 0.95) / 2. , len(tf4) - 1)
+   #md = statistics.mean(takt)
+   #deviation = sem(takt) * t.ppf((1 + 0.95) / 2. , len(takt) - 1)
 
-   estimationMean = statistics.mean(all_med)
-   estimationInterval = statistics.mean(all_dev)
-
-
-   upperEstimation = estimationMean + estimationInterval
-   bottomEstimation = estimationMean - estimationInterval
+   upperEstimation = md + deviation
+   bottomEstimation = md - deviation
    upperEstimation = datetime.timedelta(seconds=upperEstimation)
    upperEstimation = upperEstimation - datetime.timedelta(microseconds=upperEstimation.microseconds)
    bottomEstimation = datetime.timedelta(seconds=bottomEstimation)
@@ -187,8 +199,8 @@ try:
    #Duration Print
 
    print("ID:" + str(idProject[int(element)]))
-   originalEstimation = datetime.timedelta(seconds=validationEstimations[int(element)])
-   originalEstimation = originalEstimation - datetime.timedelta(microseconds=originalEstimation.microseconds)
+   # originalEstimation = datetime.timedelta(seconds=validationEstimations[int(element)])
+   # originalEstimation = originalEstimation - datetime.timedelta(microseconds=originalEstimation.microseconds)
 
    realValue = datetime.timedelta(seconds=validation[int(element)])
    realValue = realValue - datetime.timedelta(microseconds=realValue.microseconds)
@@ -199,7 +211,7 @@ try:
    # realValue = datetime.timedelta(seconds=statistics.mean(validation))
    # realValue = realValue - datetime.timedelta(microseconds=realValue.microseconds)
 
-   print("Original Estimation: " + str(originalEstimation))
+   #print("Original Estimation: " + str(originalEstimation))
    print("Real Value: " + str(realValue))
    print("Model Estimation: " + str(rangeEstimation))
 
