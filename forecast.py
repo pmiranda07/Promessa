a2c0859b80f87dd7ba9e911d197bcf01cdaed06c
Pedro <pedro21fcp@gmail.com>
2020-05-10 18:16:27 +0100

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -7,13 +7,85 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-try:
-   connection = psycopg2.connect(user="pmiranda", host="localhost", port="5432", database="issues")
-   cursor = connection.cursor()
+
+def taktTimeRandProj():
+
+   cursor.execute("SELECT project FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) =12 AND EXTRACT(YEAR FROM resolutionDate) = 2019")
+   idList = cursor.fetchall()
+   idList = [il[0] for il in idList]
+   project = np.random.choice(idList)
+   return project
+
+def taktTimeValidation(project):
+   
+   cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) = 12 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
+   validationFinish = cursor.fetchall() 
+   validationFinish = [vf[0] for vf in validationFinish]
+   validationFinish.sort()
+   validation = []
+   l = 0
+   while l < len(validationFinish)-1:
+      timeInt = validationFinish[l + 1] - validationFinish[l]
+      if(timeInt.total_seconds() < 4320000):
+         validation.append(timeInt.total_seconds())
+      l += 1
+   return validation
+
+def taktTimeProj(project):
+
+   cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) < 12 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
+   resolution = cursor.fetchall() 
+   resolution= [i[0] for i in resolution]
+   resolution.sort()
+   ts = []
+   l = 0
+   while l < len(resolution)-1:
+      timeInt = resolution[l + 1] - resolution[l]
+      if(timeInt.total_seconds() < 4320000):
+         ts.append(timeInt.total_seconds())
+      l += 1
+
+   if(len(ts) < 2):
+      raise Exception(": Not enough data")
+
+   return ts
 
 
-   ################### Validation Duration ##########################################
+def taktTimeMC(ts):
+   num_reps = 1000
+   takt = []
+   r = 0
+   while r < num_reps:
+      obj = np.random.choice(ts)
+      takt.append(obj)
+      r = r + 1
+   md = statistics.mean(takt)
+   deviation = sem(takt) * t.ppf((1 + 0.95) / 2. , len(takt) - 1)
+   return [md,deviation]
 
+
+
+def taktTimePrints(md, deviation, idProject, actualValue):
+   upperEstimation = md + deviation
+   bottomEstimation = md - deviation
+   upperEstimation = datetime.timedelta(seconds=upperEstimation)
+   upperEstimation = upperEstimation - datetime.timedelta(microseconds=upperEstimation.microseconds)
+   bottomEstimation = datetime.timedelta(seconds=bottomEstimation)
+   bottomEstimation = bottomEstimation - datetime.timedelta(microseconds=bottomEstimation.microseconds)
+
+
+   rangeEstimation = "[" + str(bottomEstimation) + " -- " + str(upperEstimation) + "]"
+
+
+   print("Project ID:" + str(idProject))
+   realValue = datetime.timedelta(seconds=actualValue)
+   realValue = realValue - datetime.timedelta(microseconds=realValue.microseconds)
+
+   print("Real Value: " + str(realValue))
+   print("Model Estimation: " + str(rangeEstimation))
+
+
+def durationValidation():
    cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput") # AND output.timeestimate IS NOT NULL")
    validationStart = cursor.fetchall() 
    cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'Done' AND b.toString = 'In Progress' INNER JOIN output ON output.id = a.idOutput") # AND output.timeestimate IS NOT NULL")
@@ -32,41 +104,15 @@ try:
    idProject = []
    resolutionDates = []
    for index, x in enumerate(validationDiff):
-      if(x.total_seconds() > 600 and x.total_seconds() < 2629743): #and estimations[index] != 0):
+      if(x.total_seconds() > 600 and x.total_seconds() < 4320000): #and estimations[index] != 0):
          validation.append(x.total_seconds())
          #validationEstimations.append(estimations[index])
          idProject.append(idList[index])
          resolutionDates.append(validationFinish[index])
-   
-   element = np.random.randint(low=0, high=len(validation))
 
-   ##################################################################################
+   return [validation, idProject, resolutionDates]
 
-   ################### Validation Takt Time ##########################################
-
-   # cursor.execute("SELECT project FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) > 9 AND EXTRACT(YEAR FROM resolutionDate) = 2019")
-   # idList = cursor.fetchall()
-   # idList = [il[0] for il in idList]
-   # project = np.random.choice(idList)
- 
-   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) > 9 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
-   # validationFinish = cursor.fetchall() 
-   # validationFinish = [vf[0] for vf in validationFinish]
-   # validationFinish.sort()
-   # validation = []
-   # idProject = []
-   # l = 0
-   # while l < len(validationFinish)-1:
-   #    timeInt = validationFinish[l + 1] - validationFinish[l]
-   #    if(timeInt.total_seconds() < 2629743):
-   #       validation.append(timeInt.total_seconds())
-   #    l += 1
-   
-
-   ##################################################################################
-
-   ####################### Duration ##########################
-
+def durationProj(element, idProject, resolutionDates):
 
    #cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput")
    cursor.execute("SELECT a.date FROM changelog a INNER JOIN changelog b ON a.idOutput = b.idOutput AND a.toString = 'In Progress' AND b.toString = 'Done' INNER JOIN output ON output.id = a.idOutput AND output.project=" + str(idProject[int(element)]))
@@ -79,75 +125,23 @@ try:
    diff = [x2 - x1 for (x1, x2) in zip(start, finish)] 
    ts = []
    for ind, x in enumerate(diff):
-      #if(x.total_seconds() > 300 and x.total_seconds() < 2629743):
-      if(x.total_seconds() > 300 and x.total_seconds() < 2629743 and finish[int(ind)] < resolutionDates[int(element)]):
+      #if(x.total_seconds() > 300 and x.total_seconds() < 4320000):
+      if(x.total_seconds() > 300 and x.total_seconds() < 4320000 and finish[int(ind)] < resolutionDates[int(element)]):
          ts.append(x.total_seconds())
-
    if(len(ts) < 2):
       raise Exception(": Not enough data")
-
-
-   ###########################################################
-
-   ################# TAKT TIME #######################
    
-   # cursor.execute("SELECT resolutionDate FROM output WHERE resolutionDate IS NOT NULL AND EXTRACT(MONTH FROM resolutionDate) < 10 AND EXTRACT(YEAR FROM resolutionDate) = 2019 AND project =" + str(project))
-   # resolution = cursor.fetchall() 
-   # resolution= [i[0] for i in resolution]
-   # resolution.sort()
-   # ts = []
-   # l = 0
-   # while l < len(resolution)-1:
-   #    timeInt = resolution[l + 1] - resolution[l]
-   #    if(timeInt.total_seconds() < 2629743):
-   #       ts.append(timeInt.total_seconds())
-   #    l += 1
+   return ts
 
-
-   ####################################################
-
-   avg = statistics.mean(ts)
-   var = statistics.pvariance(ts)
-   sDev = statistics.pstdev(ts)
-   med = statistics.median(ts)
-
-   # mean = datetime.timedelta(seconds=avg)
-   # mean = mean - datetime.timedelta(microseconds=mean.microseconds)
-   # print("Mean:", mean)
-
-
-
-   # stdev = datetime.timedelta(seconds=sDev)
-   # stdev = stdev - datetime.timedelta(microseconds=stdev.microseconds)
-   # print("STD Deviation:", stdev)
-   
-
-   # median = datetime.timedelta(seconds=med)
-   # median = median - datetime.timedelta(microseconds=median.microseconds)
-   # print("Median:", median)
-
-   # std_err = sem(ts)
-   # interval = std_err * t.ppf((1 + 0.95) / 2. , len(ts) - 1)
-   # lb = med - interval
-   # ub = med + interval
-
-   # interval = datetime.timedelta(seconds=interval)
-   # interval = interval - datetime.timedelta(microseconds=interval.microseconds)
-   # print("Interval:", interval)
-
-
-
-
-################################# Monte Carlo ###################################
+def durationMC(ts):
    num_reps = 1000
-   #takt = []
    tf1 = []    # 0 - 5 Days
    tf2 = []    # 5 - 10 Days
    tf3 = []    # 10 - 15 Days
    tf4 = []    # > 15 Days
-   for r in range(num_reps):
+   r = 0
+   while r < num_reps:
       obj = np.random.choice(ts)
-      #takt.append(obj)
       if(obj < 432000):
          tf1.append(obj)
       elif(obj >= 432000 and obj < 864000):
@@ -156,6 +150,7 @@ try:
          tf3.append(obj)
       elif(obj >= 1296000):
          tf4.append(obj)
+      r = r + 1 
    md, deviation = 0, 0
    if(len(tf1) >= len(tf2) and len(tf1) >= len(tf3) and len(tf1) >= len(tf4)):
       md = statistics.mean(tf1)
@@ -169,8 +164,10 @@ try:
    elif(len(tf4) > len(tf1) and len(tf4) > len(tf2) and len(tf4) > len(tf3)):
       md = statistics.mean(tf4)
       deviation = sem(tf4) * t.ppf((1 + 0.95) / 2. , len(tf4) - 1)
-   #md = statistics.mean(takt)
-   #deviation = sem(takt) * t.ppf((1 + 0.95) / 2. , len(takt) - 1)
+
+   return [md, deviation]
+
+def durationPrints(md, deviation, idProject, actualValue):
 
    upperEstimation = md + deviation
    bottomEstimation = md - deviation
@@ -182,49 +179,125 @@ try:
 
    rangeEstimation = "[" + str(bottomEstimation) + " -- " + str(upperEstimation) + "]"
 
-
-   # plt.hist(all_med)
-   # plt.axvline(statistics.median(all_med), color='k', linestyle='dashed', linewidth=1)
-   # min_ylim, max_ylim = plt.ylim()
-   # plt.text(statistics.median(all_med)*1.1, max_ylim*0.9, 'Mean: {:.2f}'.format(statistics.median(all_med)))
-   # plt.show()
-   
-
-   #################################################################################
-   
-   
-   
-   ########################## Print Values ########################################
-   
-   #Duration Print
-
-   print("ID:" + str(idProject[int(element)]))
+   print("ID:" + str(idProject))
    # originalEstimation = datetime.timedelta(seconds=validationEstimations[int(element)])
    # originalEstimation = originalEstimation - datetime.timedelta(microseconds=originalEstimation.microseconds)
 
-   realValue = datetime.timedelta(seconds=validation[int(element)])
+
+   realValue = datetime.timedelta(seconds=actualValue)
    realValue = realValue - datetime.timedelta(microseconds=realValue.microseconds)
-
-
-   #Takt Time Print
-   # print("Project ID:" + str(project))
-   # realValue = datetime.timedelta(seconds=statistics.mean(validation))
-   # realValue = realValue - datetime.timedelta(microseconds=realValue.microseconds)
 
    #print("Original Estimation: " + str(originalEstimation))
    print("Real Value: " + str(realValue))
    print("Model Estimation: " + str(rangeEstimation))
 
+def percentageError(actualValue, estimation):
+   percentageError = abs((actualValue - estimation)/actualValue) * 100
+   print("Percentage error: " + str(int(percentageError)) + "%")
 
-##################################################################################
+def durationMSE():
+   durValidation = durationValidation()
+   validation = durValidation[0]
+   idProject = durValidation[1]
+   resolutionDates = durValidation[2]
+   num_sims = 5
+   y_forecast = []
+   y_true = []
+   s = 0
+   while s < num_sims:
+      element = np.random.randint(low=0, high=len(idProject))
+      ts = durationProj(element, idProject , resolutionDates)
+      durationMc = durationMC(ts)
+      md = durationMc[0]
+      y_forecast.append(md)
+      y_true.append(validation[int(element)])
+      s = s + 1
+   mse = np.square(np.subtract(y_true,y_forecast)).mean() 
+   print("MSE: " + str(mse))
+
+def takttimeMSE():
+   s = 0
+   num_sims = 5
+   y_forecast = []
+   y_true = []
+   while s < num_sims:
+      idProject = taktTimeRandProj()
+      validation = taktTimeValidation(idProject)
+      ts = taktTimeProj(idProject)
+      takttimeMC = taktTimeMC(ts)
+      md = takttimeMC[0]
+      y_forecast.append(md)
+      y_true.append(statistics.mean(validation))
+      s = s + 1 
+   mse = np.square(np.subtract(y_true,y_forecast)).mean() 
+   print("MSE: " + str(mse))
+
+
+try:
+   connection = psycopg2.connect(user="pmiranda", host="localhost", port="5432", database="issues")
+   cursor = connection.cursor()
+
+   ################## TaktTime ###################
+
+   # idProject = taktTimeRandProj()
+   # validation = taktTimeValidation(idProject)
+   # ts = taktTimeProj(idProject)
+   # taktTimeMC = taktTimeMC(ts)
+   # md = taktTimeMC[0]
+   # deviation = taktTimeMC[1]
+   # actualValue = statistics.mean(validation)
+   # taktTimePrints(md,deviation,idProject,actualValue)
+
+   ###############################################
+
+   ################# Duration ####################
+
+   durationValidation = durationValidation()
+   validation = durationValidation[0]
+   idProject = durationValidation[1]
+   resolutionDates = durationValidation[2]
+   element = np.random.randint(low=0, high=len(idProject))
+   ts = durationProj(element, idProject, resolutionDates)
+   durationMC = durationMC(ts)
+   md = durationMC[0]
+   deviation = durationMC[1]
+   actualValue = validation[int(element)]
+   durationPrints(md, deviation, idProject[int(element)], actualValue)
+
+
+   ###############################################
+
+   ################# Errors ######################
+
+   #percentageError(actualValue, md)
+
+   ###############################################
+
+   ############# Mean Square Error ############
+
+   #durationMSE()
+   #takttimeMSE()
+
+   ############################################
+
+   ################# Graphs ######################
+
+
+
+   # plt.hist(array)
+   # plt.axvline(statistics.median(array), color='k', linestyle='dashed', linewidth=1)
+   # min_ylim, max_ylim = plt.ylim()
+   # plt.text(statistics.median(array)*1.1, max_ylim*0.9, 'Mean: {:.2f}'.format(statistics.median(array)))
+   # plt.show()
 
 
 
    # fig1, ax1 = plt.subplots()
    # ax1.set_title('Data Plot')
-   # ax1.boxplot(ts)
+   # ax1.boxplot(array)
    # plt.show()
 
+   ################################################
 
 
 except (Exception, psycopg2.Error) as error :
